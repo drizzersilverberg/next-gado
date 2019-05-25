ad480268a2d15cdf70e0ef6ea17ae3c1bde5ffb3
Dendi Handian <dhn@voxteneo.asia>
2019-05-25 22:29:18 +0700

    add some pages

---
+++ pages/cv.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CurriculumVitae extends React.Component {
+  render() {
+    return (
+      <h1>Curriculum Vitae page</h1>
+    )
+  }
+}
+
+export default CurriculumVitae;
