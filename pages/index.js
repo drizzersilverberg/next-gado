2d2beae2310b343669b52968de9298c69a778829
Dendi Handian <dhn@voxteneo.asia>
2019-05-25 22:33:46 +0700

    simple anchors to pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-const Index = () => {
-  return (
-    <h1> Hello from Next.js </h1>
-  )
+import React from 'react';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Index page</h1>
+        <ul>
+          <li><a href="/blogs">Blogs</a></li>
+          <li><a href="/cv">CV</a></li>
+          <li><a href="/portfolios">Portfolios</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Index;
